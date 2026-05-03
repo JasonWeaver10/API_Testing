53cbedf89f26a1d68365a193cfdcfdb507c7be19
Jason Weaver <JasonTWeaver10@gmail.com>
2026-05-03 16:36:12 -0400

    Updating API Call

--- js/app.js
+++ js/app.js
@@ -1,46 +1,46 @@
 
 console.log('js is loaded');
 
-let productPrice = '';
-let productName = '';
-let productButton = '';
-let productClass = '';
+let productPrice = document.getElementById('product-price').innerText;
+let productName = document.getElementById('product-name').innerText;
+let productButton = document.querySelector('#product-button').addEventListener('click', (event) => {
+        console.log('Button was clicked!');
+        this.sendAPIRequest();
+    });;
 
-const url = 'https://httpbin.org/post'
-const data = { productName: this.productName, productPrice: this.productPrice };
+ window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'product-view',
+            'product-name': productName, 
+            'product-price': productPrice
+        });
 
 function sendAPIRequest(){
+
+    const url = 'https://httpbin.org/post'
+    const productViewEvent = window.dataLayer.find(item => item.event === 'product-view');
+
+    const data = {
+        productName: productViewEvent?.['product-name'],
+        productPrice: productViewEvent?.['product-price']
+    };
+
+    console.log('Data:', data);
     fetch(url, {
     method: 'POST', 
     headers: {
         'Content-Type': 'application/json', 
     },
-    body: JSON.stringify(this.data), 
+    body: JSON.stringify(data), 
     })
-    .then(response => response.json()) 
+    .then(response => {
+        console.log('Response:', response); 
+    return response.json();
+    })
     .then(result => {
         console.log('Success:', result);
     })
     .catch(error => {
         console.error('Error:', error);
     });
-
 }
-
-function setupPage(){
-    this.productPrice = document.getElementById('product-price').innerText;
-    this.productName = document.getElementById('product-name').innerText;
-    this.productButton = document.querySelector('#product-button');
-    this.productClass = document.querySelector('.product-card');
-
-    this.productButton.addEventListener('click', (event) => {
-        console.log('Button was clicked!');
-        this.sendAPIRequest();
-    });
-}
-
-
-
-setupPage();
-
-
